17663559af09d8ac11ced64ae1885bab2c6d31ac
Randy <randypriv@gmail.com>
2015-05-15 00:10:02 +0800

    add pic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514152655) do
+ActiveRecord::Schema.define(version: 20150514160607) do
 
   create_table "channels", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150514152655) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "pic"
   end
 
 end
