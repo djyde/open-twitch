a05439a7e169ef47903c4f6d9ab27c28bbd138f5
Randy <randypriv@gmail.com>
2015-05-16 19:29:36 +0800

    channel on done

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,12 +1,17 @@
 class ChannelsController < ApplicationController
+
+  def edit
+    @channel = Channel.find(params[:id])
+  end
+
   def update
     @new_channel = Channel.find(params[:id]).update(new_params)
-    redirect_to "/profile/#{params[:id]}"
+    redirect_to profile_path(params[:id])
   end
 
   private
 
     def new_params
-      params.require(:channel).permit(:name,:description)
+      params.require(:channel).permit(:name,:description,:channel_on)
     end
 end
