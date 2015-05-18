fcdc5e4906127f48696c863bbeecf08da391516e
Randy <randypriv@gmail.com>
2015-05-18 16:06:03 +0800

    fix bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @games = Game.all
-    @channels = Channel.where(channel_on: true)
+    @channels = Channel.where(channel_on: true).order('updated_at DESC')
   end
 
   def about
